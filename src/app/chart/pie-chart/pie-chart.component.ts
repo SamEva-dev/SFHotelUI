b4f456df4d9413b8c8723fae629b28b3d6852dab
Sam FOKAM <samevariste@yahoo.fr>
2021-07-08 18:25:10 +0200

    Add pie chart component data

--- src/app/chart/pie-chart/pie-chart.component.ts
+++ src/app/chart/pie-chart/pie-chart.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PieChartComponent implements OnInit {
 
+  public pieChartDate:number[]=[350,450,120];
+public pieChartLabels:string[]=['XYZ Logistics', 'main St Bakery', 'Acme Hosting'];
+  public colors:any[]=[
+    {
+      backgroundColor:['#26547c','#ff6b6b','#ffd166']
+    }
+  ];
+  public pieChartType:any= 'pie';
   constructor() { }
 
   ngOnInit(): void {
