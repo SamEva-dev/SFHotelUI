f738ed92e6e312ab690843adb41c5fa0fe211683
Sam FOKAM <samevariste@yahoo.fr>
2021-07-09 11:00:07 +0200

    update server component

--- src/app/sections/server/server.component.ts
+++ src/app/sections/server/server.component.ts
@@ -10,9 +10,34 @@ export class ServerComponent implements OnInit {
 
   constructor() { }
 
+  color: string;
+  buttonText:string;
   @Input() serverInput: Server;
 
   ngOnInit(): void {
+    this.setServerAction(this.serverInput.isOnline);
+  }
+
+  setServerAction(isOnline: boolean){
+
+    if ( isOnline)
+    {
+      this.serverInput.isOnline = true;
+      this.color = "#66BB6A";
+      this.buttonText="Shut Down"
+    }
+    else{
+      this.serverInput.isOnline = false;
+      this.color='#FF6B6B';
+      this.buttonText = "Start";
+    }
+
+
+  }
+  public toggleStatus(onlineStatus:boolean){
+    console.log("server online:",onlineStatus)
+    this.serverInput.isOnline = !this.serverInput.isOnline;
+    this.setServerAction(!onlineStatus);
   }
 
 }
